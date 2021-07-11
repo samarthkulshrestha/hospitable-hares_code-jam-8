d3ab21d3976350efe8d42a0f9b4ba7342697c986
saiTama-max <vinamraj01@gmail.com>
2021-07-11 17:55:19 +0530

    add token generation and redis setup

--- api/auth.py
+++ api/auth.py
@@ -1,7 +1,18 @@
-from api import app
 import secrets
+import uuid
+
+import redis
+from flask import Response
+
+from api import app
+
+redinst = redis.Redis()
+
 
 @app.route("/join")
-def join():
-    return secrets.token_hex(64)
-
+def join() -> Response:
+    """Token generation function for join route."""
+    token = secrets.token_hex(64)
+    _id = uuid.uuid1().int
+    redinst.set(token, _id)
+    return token
