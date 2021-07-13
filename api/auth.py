d46c59bc40c85d95b21c43559be26198cc383c03
Samarth Kulshrestha <samarthkulshresth@gmail.com>
2021-07-13 14:30:57 +0530

    fixing flake8 warnings

--- api/auth.py
+++ api/auth.py
@@ -1,31 +1,32 @@
 import secrets
 import uuid
 from functools import wraps
+from typing import Callable
 
 import redis
-from flask import request, jsonify, Response
+from flask import Response, jsonify, request
 
 from api import app
 
 redinst = redis.Redis()
 
 
-def verify_decorator(f):
+def verify_decorator(f: Callable) -> Callable:
+    """Verifies the token for routes that only logged-in users are permitted to access."""
+
     @wraps(f)
-    def decorated(*args, **kwargs):
+    def decorated(*args, **kwargs) -> Response:
         if "Auth-Token" in request.headers:
             token = request.headers["Auth-Token"]
-
-        if not token:
+        else:
             return jsonify({"message": "auth token missing"})
 
         try:
             uid = redinst.get(token)
-            print(uid)
+            # print(uid)
             if not uid:
                 return jsonify({"message": "invalid token"}), 401
-
-        except:
+        except Exception:
             return jsonify({"message": "invalid token"}), 401
 
         return f(uid, *args, **kwargs)
@@ -39,11 +40,12 @@ def join() -> Response:
     token = secrets.token_hex(64)
     _id = uuid.uuid1().int
     redinst.set(token, _id)
-    return jsonify({'token': token})
+    return jsonify({"token": token})
 
 
 @app.route("/protected")
 @verify_decorator
-def post(uid):
+def protected(uid: str) -> Response:
+    """Test route."""
     uid = uid.decode("utf-8")
     return jsonify({"uid": uid})
