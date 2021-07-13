c239d3df55629f8616c520c0a3def00d4690ff62
Samarth Kulshrestha <samarthkulshresth@gmail.com>
2021-07-13 13:49:04 +0530

    verify_decorator done, verifies if the user accessing the route is logged in. created test route /protected which returns the user id if a valid token is passed in the request. jsonified responses

--- api/auth.py
+++ api/auth.py
@@ -1,12 +1,36 @@
 import secrets
 import uuid
+from functools import wraps
 
 import redis
-from flask import Response
+from flask import request, jsonify, Response
 
 from api import app
 
 redinst = redis.Redis()
+
+
+def verify_decorator(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if "Auth-Token" in request.headers:
+            token = request.headers["Auth-Token"]
+
+        if not token:
+            return jsonify({"message": "auth token missing"})
+
+        try:
+            uid = redinst.get(token)
+            print(uid)
+            if not uid:
+                return jsonify({"message": "invalid token"}), 401
+
+        except:
+            return jsonify({"message": "invalid token"}), 401
+
+        return f(uid, *args, **kwargs)
+
+    return decorated
 
 
 @app.route("/join")
@@ -15,4 +39,11 @@ def join() -> Response:
     token = secrets.token_hex(64)
     _id = uuid.uuid1().int
     redinst.set(token, _id)
-    return token
+    return jsonify({'token': token})
+
+
+@app.route("/protected")
+@verify_decorator
+def post(uid):
+    uid = uid.decode("utf-8")
+    return jsonify({"uid": uid})
