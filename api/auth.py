1f0ee5d6d788b39bffd7ee542af9f36b2a5d0bd5
saiTama-max <vinamraj01@gmail.com>
2021-07-14 20:16:52 +0530

    move posts route to posts file

--- api/auth.py
+++ api/auth.py
@@ -64,28 +64,6 @@ def users() -> Response:
     return jsonify({"users": users})
 
 
-@app.route("/posts")
-def posts() -> Response:
-    """Paginate the posts"""
-    limit = 25
-    posts = []
-    page_no = request.args.get('page_no', '1')
-    if not page_no.isnumeric():
-        return jsonify({
-            'error': "page_no should be numeric"
-        })
-
-    for post in Post.select().order_by(Post.created_at.desc()).paginate(int(page_no), limit):
-        posts.append({
-            '_id': post._id,
-            'body': post.body,
-            'creator': post.creator._id,
-            'created_at': post.created_at
-        })
-
-    return jsonify(posts)
-
-
 @app.route("/protected")
 @verify_decorator
 def protected(uid: str) -> Response:
