cad5eb04ca1281c9b6d9b9bd770728ce97a35563
Aryan-Rajoria <57455619+Aryan-Rajoria@users.noreply.github.com>
2021-07-16 06:06:13 +0530

    Update settings.py
    changes to settings.py

--- tui/view/settings.py
+++ tui/view/settings.py
@@ -1,48 +1,50 @@
-from os import name
-import asciimatics
-import __future__
-import curses
+from asciimatics.scene import Scene
 from asciimatics.screen import Screen, _CursesScreen
-from asciimatics.widgets import * 
-from asciimatics.scene import *
+from asciimatics.widgets import Divider, DropdownList, Frame, Layout
+
 
 our_data = {
     "ddl1": "",
     "ddl2": "",
-    "ddl3": "",
+    "ddl3": ""
 }
 
 options_ddl1 = ["COLOUR_BLACK = 0",
-    "COLOUR_RED = 1",
-    "COLOUR_GREEN = 2",
-    "COLOUR_YELLOW = 3",
-    "COLOUR_BLUE = 4",
-    "COLOUR_MAGENTA = 5",
-    "COLOUR_CYAN = 6",
-    "COLOUR_WHITE = 7",]
+                "COLOUR_RED = 1",
+                "COLOUR_GREEN = 2",
+                "COLOUR_YELLOW = 3",
+                "COLOUR_BLUE = 4",
+                "COLOUR_MAGENTA = 5",
+                "COLOUR_CYAN = 6",
+                "COLOUR_WHITE = 7"
+                ]
 options_ddl2 = options_ddl1
 options_ddl3 = options_ddl1
-def demo(screen):
 
+
+def settings_display(screen: _CursesScreen):
+    """
+    This method displays the settings for the thoughts in a box project.
+    There is still some options remaining, also need to know to transfer the values to other views
+    """
     setting_frame = Frame(screen, screen.height, screen.width, has_border=False, title="Setting", data=our_data)
-    layout = Layout([1,18,1])
+    layout = Layout([1, 18, 1])
 
     setting_frame.add_layout(layout)
 
-    layout.add_widget(Divider(draw_line=False, height=screen.height// 3),1)
+    layout.add_widget(Divider(draw_line=False, height=screen.height // 3), 1)
     layout.add_widget(DropdownList( [],
-            label= "foreground",
-            name= "ddl1",
-            ), 1)
+                                    label="foreground",
+                                    name="ddl1",
+                                    ), 1)
     layout.add_widget(DropdownList( [],
-            label= "background",
-            name= "ddl2",
-            ), 1)
+                                    label="background",
+                                    name="ddl2",
+                                    ), 1)
     layout.add_widget(DropdownList( [],
-            label= "text",
-            name= "ddl3",
-            ), 1)
-    
+                                    label="text",
+                                    name="ddl3",
+                                    ), 1)
     setting_frame.fix()
 
     scene_list = [
@@ -51,4 +53,5 @@ def demo(screen):
 
     screen.play(scene_list)
 
-Screen.wrapper(demo)
+
+Screen.wrapper(settings_display)
