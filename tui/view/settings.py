42b578b64af66befc278f50c2feac49d57daead1
Aryan-Rajoria <57455619+Aryan-Rajoria@users.noreply.github.com>
2021-07-16 22:27:03 +0530

    Setting.py changing to class
    Changing Settings function to class(Frame)

--- tui/view/settings.py
+++ tui/view/settings.py
@@ -1,10 +1,7 @@
-# flake8: noqa
-
-from asciimatics.scene import Scene
 from asciimatics.screen import Screen
 from asciimatics.widgets import Divider, DropdownList, Frame, Layout
 
-our_data = {
+settings_data = {
     "ddl1": "",
     "ddl2": "",
     "ddl3": ""
@@ -23,36 +20,58 @@ options_ddl2 = options_ddl1
 options_ddl3 = options_ddl1
 
 
-def settings_display(screen):
+class Settings(Frame):
+
     """
-    This method displays the settings for the thoughts in a box project.
-    There is still some options remaining, also need to know to transfer the values to other views
+    This class will display the setting pane.
+
+    If you want to get access to data you can use the `get_settings` function or access `settings_data` dictionary.
     """
-    setting_frame = Frame(screen, screen.height, screen.width, has_border=False, title="Setting", data=our_data)
-    layout = Layout([1, 18, 1])
 
-    setting_frame.add_layout(layout)
+    def __init__(self, screen: Screen) -> None:
+        global settings_data
+        super().__init__(screen, screen.height, screen.width,
+                         has_border=False, title="Setting", data=settings_data, name="settings_form")
+        layout = Layout([1, 18, 1])
 
-    layout.add_widget(Divider(draw_line=False, height=screen.height // 3), 1)
-    layout.add_widget(DropdownList( [],
-                                    label="foreground",
-                                    name="ddl1",
-                                    ), 1)
-    layout.add_widget(DropdownList( [],
-                                    label="background",
-                                    name="ddl2",
-                                    ), 1)
-    layout.add_widget(DropdownList( [],
-                                    label="text",
-                                    name="ddl3",
-                                    ), 1)
-    setting_frame.fix()
+        self.add_layout(layout)
 
-    scene_list = [
-        Scene([setting_frame], -1, name="Settings")
-    ]
+        layout.add_widget(Divider(draw_line=False, height=screen.height // 3), 1)
+        layout.add_widget(DropdownList([(x, i) for i, x in enumerate(options_ddl1)],
+                                       label="foreground",
+                                       name="fg",
+                                       on_change=self._on_change,
+                                       ), 1)
+        layout.add_widget(DropdownList([(x, i) for i, x in enumerate(options_ddl2)],
+                                       label="background",
+                                       name="bg",
+                                       on_change=self._on_change,
+                                       ), 1)
+        layout.add_widget(DropdownList([(x, i) for i, x in enumerate(options_ddl3)],
+                                       label="text",
+                                       name="txt",
+                                       on_change=self._on_change,
+                                       ), 1)
+        self.fix()
 
-    screen.play(scene_list)
+    def _on_change(self) -> None:
+        self.save()
+        global settings_data
+        settings_data = self.data
 
 
-Screen.wrapper(settings_display)
+def get_settings() -> dict:
+    """This will return the settings data collected"""
+    return settings_data
+
+# while True:
+#     def demo(screen):
+#         x = Scene([Settings(screen)], 150, name="Settings")
+#         screen.play([x], repeat=False)
+#         # st = settings_data["ddl1"]+', '+settings_data["ddl2"]+', '+settings_data["ddl3"]
+#         screen.clear()
+#         screen.refresh()
+#         screen.print_at(settings_data,0,0)
+#         screen.refresh()
+#         sleep(10)
+#     Screen.wrapper(demo)
