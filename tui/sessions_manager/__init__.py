40e6bee106b531a590f57dfef306f884bb3355ea
Aryan-Rajoria <aryanrajoria1003@gmail.com>
2021-07-17 01:59:49 +0530

    Session Manager module

---
+++ tui/sessions_manager/__init__.py
@@ -0,0 +1,2 @@
+# token is physical file - encrypted and valid for certain time as dictated by the server
+# nothing to run for now in init
---
+++ tui/sessions_manager/auth_request.py
@@ -0,0 +1,5 @@
+import requests
+
+webname = "localhost:8000"
+
+r = requests.get(webname,)
