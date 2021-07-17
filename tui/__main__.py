787b2221b23b3e9874adace38c1b8d7d28e95ffb
Aryan-Rajoria <57455619+Aryan-Rajoria@users.noreply.github.com>
2021-07-17 15:47:53 +0530

    adding setting screen

--- tui/__main__.py
+++ tui/__main__.py
@@ -1,10 +1,11 @@
 import sys
+from tui.view.settings import Settings
 
 from asciimatics.exceptions import ResizeScreenError
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
 
-from tui.view.box_page import BoxPage
+from tui.view.box_page import BoxPage, NewBoxPage
 from tui.view.chat import ChatPage
 from tui.view.home import HomePage
 
@@ -16,7 +17,10 @@ class BoxSelection(object):
 
         # List of dicts, where each dict contains a single contact, containing
         # name, address, phone, email and notes fields.
-        self.boxes = []
+        self.boxes = dict()
+    
+    def refresh():
+        pass
 
 chat_data = {
     "chat": ["This is the first text", "This is the second text"],
@@ -28,12 +32,14 @@ def main(screen: Screen, scene: Scene) -> None:
     """The class's docstring"""
 
     boxselection = BoxSelection()
+    #boxselection.refresh()
 
     scenes = [
         Scene([HomePage(screen)], -1, name="HomePage"),
         Scene([BoxPage(screen, box_selection=boxselection)], -1, name="BoxPage"),
         Scene([ChatPage(screen, chat_data=chat_data)], -1, name="ChatPage"),
-
+        Scene([NewBoxPage(screen, boxselection)], -1, name="NewBoxPage"),
+        Scene([Settings(screen)], -1, name="Settings"),
     ]
     screen.play(scenes, stop_on_resize=True, start_scene=scene, allow_int=True)
 
