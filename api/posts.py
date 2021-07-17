38c365527049e4ad94a8e6ae2a08b0cecb734ee2
Samarth Kulshrestha <samarthkulshresth@gmail.com>
2021-07-17 19:07:34 +0530

    changing all occurences of request.json into request.args.get

--- api/posts.py
+++ api/posts.py
@@ -13,7 +13,7 @@ def posts() -> Response:
     """Paginate the posts"""
     limit = 25
     posts = []
-    box = Box.get(Box._id == request.json["box_id"])
+    box = Box.get(Box._id == request.args.get("box_id"))
     if not box:
         return jsonify({"message": "invalid box id"})
     page_no = request.args.get("page_no", "1")
@@ -43,12 +43,12 @@ def post(uid: str) -> Response:
     """Route for adding a post to the database."""
     user = User.select().where(User._id == uid).get()
     _id = uuid.uuid1().int
-    box = Box.select().where(Box._id == request.json["box_id"])
+    box = Box.select().where(Box._id == request.args.get("box_id"))
     if not box:
         return jsonify({"message": "invalid box id"})
     post = Post.create(
         _id=str(_id),
-        body=request.json["body"],
+        body=request.args.get("body"),
         creator=user,
         box=box,
         created_at=datetime.now(),
@@ -64,7 +64,10 @@ def new_box(uid: str) -> Response:
     user = User.select().where(User._id == uid).get()
     _id = uuid.uuid1().int
     box = Box.create(
-        _id=str(_id), name=request.json["name"], creator=user, created_at=datetime.now()
+        _id=str(_id),
+        name=request.args.get("name"),
+        creator=user,
+        created_at=datetime.now(),
     )
     box.save()
     return jsonify({"box_id": box._id, "name": box.name})
@@ -80,9 +83,7 @@ def boxes() -> Response:
         return jsonify({"error": "page_no should be numeric"})
 
     for box in (
-        Box.select()
-        .order_by(Box.created_at.desc())
-        .paginate(int(page_no), limit)
+        Box.select().order_by(Box.created_at.desc()).paginate(int(page_no), limit)
     ):
         boxes.append(
             {
